de3229afe66938beac773b51cb30aba7e6404c62
Azarill <heierms@gmail.com>
2022-05-25 22:08:06 -0500

    starter code

---
+++ server.js
@@ -0,0 +1,24 @@
+// initilze server
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+// ------------------- MIDDLEWARE -------------------|
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// Routes
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+// PORTS
+const PORT = process.env.PORT || 3001;
+
+
+
+
+//LISTENS FOR REQUESTS ON SELECTED PORT
+  app.listen(PORT, () => {
+   console.log(`App listening at http://localhost:${PORT} 🚀`);
+  });
