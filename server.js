8067c5cec19174521840ace06c3764d1649a77f5
Azarill <heierms@gmail.com>
2022-05-27 00:57:21 -0500

    update hero

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // initilze server
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -11,6 +10,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
+app.use(express.static('public'));
 app.use(express.json());
 // Routes
 app.use('/api', apiRoutes);
